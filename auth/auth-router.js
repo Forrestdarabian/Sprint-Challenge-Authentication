f18873550b37202e9cf37f30625579e7e48d68cb
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-11-15 08:21:32 -0800

    Register and Login coded, now onto the middleware login uses

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,35 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  Users.insert({ username, password: bcrypt.hashSync(password, 8) })
+    .then(id => {
+      res.status(201).json({ message: "User registered", id });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Error registering user" });
+    });
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  Users.findByUsername(username)
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: "Login Successful!",
+          token
+        });
+      } else {
+        res.status(401).json({ message: "Invalid password" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Error logging in user" });
+    });
 });
 
 module.exports = router;
