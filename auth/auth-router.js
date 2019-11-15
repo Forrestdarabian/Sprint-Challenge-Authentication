b030b5c01a5e93902a0c13000ac5704af185f812
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-11-15 08:52:55 -0800

    Reworking some stuff

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,7 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Users = require("./auth-model.js");
 
 router.post("/register", (req, res) => {
   const { username, password } = req.body;
